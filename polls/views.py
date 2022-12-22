16fa29dd6d46520da342d188ae95fa8d0d2c0901
Anirudh-rao <anirudh131299@gmail.com>
2022-12-22 20:33:12 +0530

    Some Changes in Project

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from .models import Question
 
+def detail(request, question_id):
+    return  HttpResponse("Yout looking at the Question %s" %question_id)
+
+def results(request,  question_id):
+    response = "Your Looking at the Results of Question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Your Voting on Question "%question_id)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
